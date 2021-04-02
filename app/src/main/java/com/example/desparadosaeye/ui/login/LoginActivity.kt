141b8945b32b911e13e6fb4e537c63a4caf7f6c6
Jacob Valdez <jacobfv@msn.com>
2021-04-02 09:11:16 -0500

    shortcircuited login to main

--- app/src/main/java/com/example/desparadosaeye/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/desparadosaeye/ui/login/LoginActivity.kt
@@ -1,12 +1,16 @@
 package com.example.desparadosaeye.ui.login
 
 import android.app.Activity
+import android.content.Intent
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 import com.example.desparadosaeye.R
+import com.example.desparadosaeye.ui.MainActivity
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,5 +18,13 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        // makes view resize when keyboard appears
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+    }
+
+    fun onLoginButtonPress(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
